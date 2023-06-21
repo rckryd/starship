2f189192655b4ef88ade768dbf7fac1997467567
Ricky Riyadi <rckryd@gmail.com>
2023-06-22 06:19:29 +0700

    init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,vue}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
